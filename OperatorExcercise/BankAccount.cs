7cd6e79a71ed0c39dd1d65114121d0f9d915d14a
IVProb <jnoojinjr@gmail.com>
2021-03-22 20:21:41 -0500

    Initialized: First Commit

---
+++ OperatorExcercise/BankAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace OperatorExcercise
+{
+    public class BankAccount
+    {
+        public BankAccount()
+        {
+        }
+
+        //Fields
+        private double _balance = 0;
+
+        public void Deposit(double amount)
+        {
+            _balance += amount;  // _balance = _balance + amount;
+        }
+
+        public void GetBalance()
+        {
+            Console.WriteLine($"Account Balance: {_balance}");
+        }
+
+    }
+}
---
+++ OperatorExcercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OperatorExcercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount mine = new BankAccount();
+
+            double deposit;
+
+            Console.WriteLine("How much do you want to deposit?");
+
+            deposit = double.Parse(Console.ReadLine());
+
+            mine.Deposit(deposit);
+
+            mine.GetBalance();
+
+
+        }
+    }
+}
